4a43cc801be1fee2166a1b55015e8ef1b80d66b7
rogerSC83 <60799991+rogerSC83@users.noreply.github.com>
2020-02-21 20:27:52 -0300

    Update Exercicio_7.java
    Correção

--- Exercicio_7.java
+++ Exercicio_7.java
@@ -1,19 +1,20 @@
-class Exercicio_7
+class Exercicio_7{
     public static void main(String[] args) {
-        nota1 = 8;
-        nota2 = 9;
-        media = (nota1+nota2)/2;
+        int nota1 = 10;
+        int nota2 = 10;
+        int media = (nota1 + nota2) / 2;
 
-        if (media == 10) {
-            System.out.println("Aprovado com Distinção!!!");            
-        }
-        else if ( media <10 && media >= 7) {
+        if (media == 10) 
+            System.out.println("Aprovado" + " com Distinção!!!");            
+        
+        else if ( media <10 && media >= 7) 
             System.out.println("Aprovado!"); 
-        }
+        
         else 
             System.out.println("Reprovado.");
-        }
+        
         
     }
+}
         
-    }
+    
