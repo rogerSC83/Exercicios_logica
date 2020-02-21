3dee1af31054c3f7aad9c8a504765d3bf9311857
rogerSC83 <60799991+rogerSC83@users.noreply.github.com>
2020-02-21 20:02:29 -0300

    Exercicio_7
    Calculando a media das notas de um aluno

---
+++ Exercicio_7.java
@@ -0,0 +1,19 @@
+class Exercicio_7
+    public static void main(String[] args) {
+        nota1 = 8;
+        nota2 = 9;
+        media = (nota1+nota2)/2;
+
+        if (media == 10) {
+            System.out.println("Aprovado com Distinção!!!");            
+        }
+        else if ( media <10 && media >= 7) {
+            System.out.println("Aprovado!"); 
+        }
+        else 
+            System.out.println("Reprovado.";)
+        }
+        
+    }
+        
+    }
